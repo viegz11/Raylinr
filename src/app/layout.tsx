8d8ff1fed45f12d8cb88ee93417bfed16d14a973
Vercel <vercel[bot]@users.noreply.github.com>
2026-05-19 13:52:41 +0000

    Install Vercel Web Analytics
    This is an automated pull request generated by Vercel to help install [Web Analytics](https://vercel.com/docs/analytics) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -25,6 +26,7 @@ export default function RootLayout({
         <Navbar />
         <main className="flex-grow flex flex-col">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
